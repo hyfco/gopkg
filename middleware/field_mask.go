8d533c5f77a00fd2b0f6da2deec89553bb0cfbba
jerryzhou <jerryzhou628@gmail.com>
2024-01-17 09:35:27 +0800

    feat(middleware):add midlleware

--- middleware/field_mask.go
+++ middleware/field_mask.go
@@ -1,1 +1,24 @@
 package middleware
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/hyfco/gopkg/fieldmask"
+	"google.golang.org/protobuf/proto"
+)
+
+func ResponseFieldMask(visibility fieldmask.Visibility) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			reply, err = handler(ctx, req)
+			if err != nil {
+				return
+			}
+			if resp, ok := reply.(proto.Message); ok {
+				fieldmask.Filter(resp, visibility)
+			}
+			return
+		}
+	}
+}
--- middleware/validate.go
+++ middleware/validate.go
@@ -2,62 +2,25 @@ package middleware
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-kratos/kratos/v2/errors"
-	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
-	"github.com/go-kratos/kratos/v2/transport"
 )
 
 type Validate interface {
 	ValidateAll() error
 }
 
-// Redacter defines how to log an object
-type Redacter interface {
-	Redact() string
-}
-
-// extractArgs returns the string of the req
-func extractArgs(req interface{}) string {
-	if redacter, ok := req.(Redacter); ok {
-		return redacter.Redact()
-	}
-	if stringer, ok := req.(fmt.Stringer); ok {
-		return stringer.String()
-	}
-	return fmt.Sprintf("%+v", req)
-}
-
-func ParameterValidate(logger log.Logger) middleware.Middleware {
+func ParameterValidate() middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
-			var (
-				kind      string
-				operation string
-			)
-
-			if info, ok := transport.FromServerContext(ctx); ok {
-				kind = info.Kind().String()
-				operation = info.Operation()
-			}
-
 			if wrapper, ok := req.(Validate); ok {
 				err = wrapper.ValidateAll()
 				if err != nil {
 					err = errors.New(400, "InvalidArgument", err.Error())
 					return
 				}
-			} else {
-				_ = log.WithContext(ctx, logger).Log(log.LevelInfo,
-					"kind", "server",
-					"component", kind,
-					"operation", operation,
-					"args", extractArgs(req),
-				)
 			}
-
 			return handler(ctx, req)
 		}
 	}
